ad9dac40c27dc15a5723d51529204f2152048763
etrauber <emma.trauber@gmail.com>
2023-04-20 13:52:32 -0400

    adding all changes

---
+++ NimRunner.java
@@ -0,0 +1,24 @@
+public class NimRunner{
+    boolean player;
+    int numPieces;
+
+    public static boolean runGame(){
+        return true;
+    }
+
+    public statin int minimax(int state, boolean myTurn){
+        return 1; //return 1 or -1 to represent good or bad for me
+        //minimize if player y turn
+        //maximise if player x turn
+    }
+
+    public static int getXMove(int state){
+        //determine how many pieces on the board
+        //pass through this state and who's turn through minimax and determine if playerx or playery
+        //determine how many pieces you are taking
+    }
+
+    public static int getYMove(state){
+
+    }
+}
