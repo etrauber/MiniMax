a39c08215cd7fa5a25b85fc3ba53818080f5b58e
etrauber <emma.trauber@gmail.com>
2023-04-23 14:40:30 -0400

    adding updated code with wokring minimax and best move

--- NimRunner.java
+++ NimRunner.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class NimRunner{
     boolean player;
     int numPieces;
@@ -5,20 +6,52 @@ public class NimRunner{
     public static boolean runGame(){
         return true;
     }
-
-    public statin int minimax(int state, boolean myTurn){
-        return 1; //return 1 or -1 to represent good or bad for me
-        //minimize if player y turn
-        //maximise if player x turn
+    
+    public static int bestMove(int numPieces, boolean turn){
+        int move = 1;
+        for(int pieces=1; pieces<=3; pieces++){
+            if(numPieces < pieces){
+                int  score = minimax(numPieces-pieces,!turn);
+                if(score > 0){
+                    move = pieces;
+                    break;
+                }
+            }
+        }
+        return move;
     }
 
-    public static int getXMove(int state){
-        //determine how many pieces on the board
-        //pass through this state and who's turn through minimax and determine if playerx or playery
-        //determine how many pieces you are taking
+    public static int minimax(int state, boolean myTurn){
+        if(state == 0){
+            if (myTurn == false){
+                return -1;
+            } else {
+                return 1;
+            }
+        } else {
+            ArrayList<Integer> scores = new ArrayList<>();
+            for(int pieces=1; pieces<=3; pieces++){
+                if(pieces<=state){
+                    int score = minimax(state-pieces, !myTurn);
+                    scores.add(score);
+                } 
+            }
+            System.out.println(scores);
+            if(myTurn){
+                return Collections.max(scores);
+            } else {
+                return Collections.min(scores);
+            }
+        }
     }
 
-    public static int getYMove(state){
+    // public static int getXMove(int state){
+    //     //determine how many pieces on the board
+    //     //pass through this state and who's turn through minimax and determine if playerx or playery
+    //     //determine how many pieces you are taking
+    // }
 
-    }
+    // public static int getYMove(state){
+
+    // }
 }
