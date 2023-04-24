e522b4ade0aa1e736cedc62b42411678b4d20235
etrauber <emma.trauber@gmail.com>
2023-04-23 22:57:30 -0400

    added a test case to test best move

--- Tests.java
+++ Tests.java
@@ -1,5 +1,7 @@
 public class Tests{
     public static void main(String[] args){
-        
+        System.out.println("Testing my NimRunner class.");
+        System.out.println(NimRunner.minimax(4, true) == 1);
+        System.out.println(NimRunner.bestMove(4, true));
     }
 }
